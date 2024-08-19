4a5b174dc17acebf726c7fa4d091ab6387aae858
Md Ain Uddin Ahmmad <real.world.developer@gmail.com>
2024-08-19 03:29:49 +0100

    Update light.js

--- light.js
+++ light.js
@@ -10,8 +10,8 @@ const searchBtn = document.getElementById('serach-btn');
 
 
 async function displayData(cityname) {
-  const promise = await fetch((`http://api.weatherapi.com/v1/current.json?key=f0d57e9008e747adba015149241908&q={cityname}&aqi=no
-  `));
+  const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=f0d57e9008e747adba015149241908&q=${cityname}&aqi=no
+  `);
   return await promise.json()
 }
 
